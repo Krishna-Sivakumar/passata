a083afb7a8d67565ff3b4b126365b5a84ce595a7
Razvan <razvan@razshare.dev>
2025-03-07 06:57:35 +0100

    feat(server,db): avoiding long polling and making sure db initialization statements are executed in order.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,47 +1,127 @@
 import { LogEvent } from "./structs";
 import sqlite3 from "sqlite3";
 
-export async function setupDB(): Promise<sqlite3.Database> {
-    let db = new sqlite3.Database("../passata.sqlite3");
-    db.run("PRAGMA journal_mode=WAL;")
-    db.run("CREATE TABLE IF NOT EXISTS log (token TEXT, log JSONB, timestamp INTEGER DEFAULT CURRENT_TIMESTAMP, connection TEXT);")
-    db.run("CREATE INDEX IF NOT EXISTS token_index ON log(token);")
-    db.run("CREATE INDEX IF NOT EXISTS timestamp_index ON log(timestamp);")
-    return db;
+let dbInstance: sqlite3.Database;
+
+export function database(): Promise<sqlite3.Database> {
+  return new Promise((resolve, reject) => {
+    if (dbInstance) {
+      resolve(dbInstance);
+      return;
+    }
+
+    dbInstance = new sqlite3.Database("../passata.sqlite3");
+    dbInstance.run("PRAGMA journal_mode=WAL;", (_: any, err: Error | null) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      dbInstance.run(
+        "CREATE TABLE IF NOT EXISTS log (token TEXT, log JSONB, timestamp INTEGER DEFAULT CURRENT_TIMESTAMP, connection TEXT);",
+        (_: any, err: Error | null) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          dbInstance.run(
+            "CREATE INDEX IF NOT EXISTS token_index ON log(token);",
+            (_: any, err: Error | null) => {
+              if (err) {
+                reject(err);
+                return;
+              }
+              dbInstance.run(
+                "CREATE INDEX IF NOT EXISTS timestamp_index ON log(timestamp);",
+                (_: any, err: Error | null) => {
+                  if (err) {
+                    reject(err);
+                    return;
+                  }
+                  resolve(dbInstance);
+                },
+              );
+            },
+          );
+        },
+      );
+    });
+  });
 }
 
 export async function fetchRow<T>(statement: sqlite3.Statement): Promise<T> {
-    return new Promise((resolve, reject) => {
-        statement.get((err, row) => {
-            if (err) {
-                reject(err)
-            }
-            resolve(row as T)
-        })
-    })
+  return new Promise((resolve, reject) => {
+    statement.get((err, row) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(row as T);
+    });
+  });
 }
 
-export async function fetchRows<T>(statement: sqlite3.Statement, params: any[]): Promise<T[]> {
-    return new Promise((resolve, reject) => {
-        statement.all(params, (err, rows) => {
-            if (err) {
-                reject(err)
-            }
-            resolve(rows as T[])
-        })
-    })
+export async function fetchRows<T>(
+  statement: sqlite3.Statement,
+  params: any[],
+): Promise<T[]> {
+  return new Promise((resolve, reject) => {
+    statement.all(params, (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows as T[]);
+    });
+  });
 }
 
-export async function insertLog(token: string, log: LogEvent, connectionToken: string) {
-    try {
-        let db = await setupDB();
-        let record = await fetchRow<{log: string, connection: string}>(db.prepare("SELECT log, connection FROM log ORDER BY timestamp DESC LIMIT 1"))
-        if (record.log && (JSON.parse(record.log) as LogEvent).state == log.state && connectionToken != record.connection) {
-            // we don't process identical logs from multiple sources.
-            return;
-        }
-        db.prepare("INSERT INTO log(log, token, timestamp, connection) VALUES(?,?,?,?)").run(JSON.stringify(log), token, Date.now(), connectionToken);
-    } catch (e) {
-        console.log(e)
+export async function findLogs(
+  token: string,
+  last_timestamp: number,
+): Promise<Array<{ log: string; timestamp: number }>> {
+  const db = await database();
+  if (last_timestamp == -1) {
+    const statement = db.prepare(
+      "SELECT log, timestamp FROM log WHERE token = ? AND timestamp > ? ORDER BY timestamp DESC",
+    );
+    return await fetchRows(statement, [token, last_timestamp]);
+  }
+  const statement = db.prepare(
+    "SELECT log, timestamp FROM log WHERE token = ? AND timestamp > ? AND connection <> ? ORDER BY timestamp DESC",
+  );
+  return await fetchRows(statement, [token, last_timestamp]);
+}
+
+export async function insertLog(
+  token: string,
+  log: LogEvent,
+  connectionToken: string,
+) {
+  try {
+    const db = await database();
+    const selectStatement = db.prepare(
+      "SELECT log, connection FROM log ORDER BY timestamp DESC LIMIT 1",
+    );
+    let record = await fetchRow<{ log: string; connection: string }>(
+      selectStatement,
+    );
+    if (
+      record &&
+      record.log &&
+      (JSON.parse(record.log) as LogEvent).state == log.state &&
+      connectionToken != record.connection
+    ) {
+      // we don't process identical logs from multiple sources.
+      return;
     }
+    const insertStatement = db.prepare(
+      "INSERT INTO log(log, token, timestamp, connection) VALUES(?,?,?,?)",
+    );
+    insertStatement.run(
+      JSON.stringify(log),
+      token,
+      Date.now(),
+      connectionToken,
+    );
+  } catch (e) {
+    console.log(e);
+  }
 }
